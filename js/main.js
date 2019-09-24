cc0f9f3061e4159f452baea72dc7b2b5f4c09964
Egrik <egor-office-mail@yandex.ru>
2019-09-24 23:12:00 +0300

    Added more constants and did a little refactoring

--- js/main.js
+++ js/main.js
@@ -5,72 +5,72 @@ var CHECKIN = ['12:00', '13:00', '14:00'];
 var CHECKOUT = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var BODY_WIDTH = 1200;
+var PIN_WIDTH = 50;
+var PIN_HEIGHT = 70;
+var MIN_PRICE = 500;
+var MAX_PRICE = 10000;
+var MIN_ROOMS = 1;
+var MAX_ROOMS = 4;
+var MIN_QUESTS = 1;
+var MAX_QUESTS = 8;
+var MIN_X = 0;
+var MIN_Y = 130;
+var MAX_Y = 630;
+var QUANTITY_PINS = 8;
 var advTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 var mapBlock = document.querySelector('.map');
 var blockMapPins = mapBlock.querySelector('.map__pins');
-var body = 1200;
-var pinWidth = 50;
-var pinHeight = 70;
 
 var getRandomNum = function (min, max) {
   var rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
 };
 
-var generateRandomArr = function (arr) {
-  var newArr = [];
-
-  for (var currentNum = 0; currentNum < arr.length; currentNum++) {
-    var num = getRandomNum(currentNum, arr.length - 1);
-    newArr.push(arr[num]);
-    currentNum = num;
-  }
-
-  return newArr;
-};
-
 var generateAds = function (quantity) {
   var ads = [];
 
   for (var i = 0; i < quantity; i++) {
-    ads.push({
+    var obj = {
       author: {
         avatar: 'img/avatars/user' + '0' + (i + 1) + '.png'
       },
 
       offer: {
         title: 'Сдаётся супер-пупер помещение',
-        price: getRandomNum(500, 10000),
+        features: FEATURES.slice(getRandomNum(0, FEATURES.length - 1)),
+        photos: PHOTOS.slice(getRandomNum(0, PHOTOS.length - 1)),
+        price: getRandomNum(MIN_PRICE, MAX_PRICE),
         type: TYPE_ROOM[getRandomNum(0, TYPE_ROOM.length - 1)],
-        rooms: getRandomNum(1, 4),
-        guests: getRandomNum(1, 8),
+        rooms: getRandomNum(MIN_ROOMS, MAX_ROOMS),
+        guests: getRandomNum(MIN_QUESTS, MAX_QUESTS),
         checkin: CHECKIN[getRandomNum(0, CHECKIN.length - 1)],
         checkout: CHECKOUT[getRandomNum(0, CHECKOUT.length - 1)],
         description: 'Всё очень здорово, берите не пожалеете!'
       },
 
       location: {
-        x: getRandomNum(0, body),
-        y: getRandomNum(130, 630)
-      }});
+        x: getRandomNum(MIN_X, BODY_WIDTH),
+        y: getRandomNum(MIN_Y, MAX_Y)
+      }};
 
-    ads[i].offer.address = ads[i].location.x + ', ' + ads[i].location.y;
-    ads[i].offer.features = generateRandomArr(FEATURES);
-    ads[i].offer.photos = generateRandomArr(PHOTOS);
+    obj.offer.address = obj.location.x + ', ' + obj.location.y;
+
+    ads.push(obj);
   }
 
   return ads;
 };
 
-var ads = generateAds(8);
+var ads = generateAds(QUANTITY_PINS);
 
 mapBlock.classList.remove('map--faded');
 
 var createAdv = function (arrItem) {
   var advElement = advTemplate.cloneNode(true);
 
-  advElement.style.left = arrItem.location.x - (pinWidth / 2) + 'px';
-  advElement.style.top = arrItem.location.y - pinHeight + 'px';
+  advElement.style.left = arrItem.location.x - (PIN_WIDTH / 2) + 'px';
+  advElement.style.top = arrItem.location.y - PIN_HEIGHT + 'px';
   advElement.querySelector('img').src = arrItem.author.avatar;
   advElement.querySelector('img').alt = arrItem.offer.title;
 
